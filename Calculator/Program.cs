09435c04bbae7f9afccb113a0a082ea75c8aada1
labstraction <45163263+labstraction@users.noreply.github.com>
2021-11-10 12:19:19 +0100

    pubblicata  e testata vers 1.0

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,14 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string numeroUnoInStringa = args[0];
+            string numeroDueInStringa = args[1];
+            int numeroUno = Convert.ToInt32(numeroUnoInStringa);
+            int numeroDue = Convert.ToInt32(numeroDueInStringa);
+            int somma = numeroUno + numeroDue;
+            Console.WriteLine("La somma è: " + somma);
+
+            // Console.WriteLine("La somma è: " + (Convert.ToInt32(args[0]) + Convert.ToInt32(args[1])));
         }
     }
 }
